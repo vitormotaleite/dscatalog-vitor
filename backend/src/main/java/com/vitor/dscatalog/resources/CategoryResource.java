acfed678b5e5f1dde799ee58583c0d85b51574d5
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-26 17:34:13 -0300

    h2 database

--- backend/src/main/java/com/vitor/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/vitor/dscatalog/resources/CategoryResource.java
@@ -1,24 +1,28 @@
 package com.vitor.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vitor.dscatalog.entities.Category;
+import com.vitor.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L,"Livros"));
-		list.add(new Category(2L, "Eletronicos"));
+		
+		List<Category> list = service.findAll();
+		
 		return ResponseEntity.ok().body(list);
 	}
 	
