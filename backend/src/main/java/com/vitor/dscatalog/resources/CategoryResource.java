31015867d149eed651e9b488a699f1abdd644f0e
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-27 10:29:48 -0300

    DTO

--- backend/src/main/java/com/vitor/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/vitor/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vitor.dscatalog.entities.Category;
+import com.vitor.dscatalog.dto.CategoryDTO;
 import com.vitor.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,9 +19,9 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
