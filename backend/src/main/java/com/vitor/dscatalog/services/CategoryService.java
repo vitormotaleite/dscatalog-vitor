f02972ad894e64e17dbe239e9a8a3f732ae54a90
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-30 14:43:14 -0300

    refatoramento no pageable

--- backend/src/main/java/com/vitor/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/vitor/dscatalog/services/CategoryService.java
@@ -1,13 +1,14 @@
 package com.vitor.dscatalog.services;
 
 import java.util.Optional;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,8 +25,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Category> list = repository.findAll(pageRequest);
+	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
+		Page<Category> list = repository.findAll(pageable);
 		return list.map(x -> new CategoryDTO(x));
 		
 	}
