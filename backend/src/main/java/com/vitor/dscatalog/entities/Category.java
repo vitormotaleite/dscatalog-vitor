26f17057af7a374aa70126492b4686010fdd1018
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2023-01-12 16:05:54 -0300

    user e role feitos

--- backend/src/main/java/com/vitor/dscatalog/entities/Category.java
+++ backend/src/main/java/com/vitor/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.vitor.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,6 +32,9 @@ public class Category implements Serializable{
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+	
 	public Category() {
 		
 	}
@@ -37,6 +43,10 @@ public class Category implements Serializable{
 		super();
 		this.id = id;
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	public Long getId() {
