64dec4a7c51a0c7dc6179af19cfe6a93b0978387
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2023-01-23 10:05:31 -0300

    oauth2 implementado

--- backend/src/main/java/com/vitor/dscatalog/services/UserService.java
+++ backend/src/main/java/com/vitor/dscatalog/services/UserService.java
@@ -3,11 +3,16 @@ package com.vitor.dscatalog.services;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +28,11 @@ import com.vitor.dscatalog.repositories.UserRepository;
 import com.vitor.dscatalog.services.exception.DatabaseException;
 import com.vitor.dscatalog.services.exception.ResourceNotFoundException;
 
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
@@ -94,5 +102,16 @@ public class UserService {
 			throw new DatabaseException("violacao de integridade");
 		}
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(user == null) {
+			logger.error("usuario nao encontrado: " + username);
+			throw new UsernameNotFoundException("email nao encontrado");
+		}
+		logger.info("usuario encontrado: " + username);
+		return user;
 	}
 }
